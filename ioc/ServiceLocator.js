1e92e8d858b982339a9eaa78ac952f10b1970f25
this_lucky <ylp1127@126.com>
2017-05-06 22:07:47 +0800

    change js

--- ioc/ServiceLocator.js
+++ ioc/ServiceLocator.js
@@ -3,3 +3,32 @@
  * @license http://www.apache.org/licenses/LICENSE-2.0
  */
 'use strict';
+
+/**
+ * 服务定位器
+ */
+class ServiceLocator {
+    
+    /**
+     * constructor
+     */
+    constructor() {
+        /**
+         * @property {Map} services
+         */
+        this.services = new Map();
+    }
+    
+    /**
+     * 注册一个服务
+     *
+     */
+    setService(key, service) {
+        
+    }
+    
+    getService(key) {}
+    
+}
+
+module.exports = ServiceLocator;
