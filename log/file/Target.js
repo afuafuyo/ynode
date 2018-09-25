001723c86489f8292b64119f66fbd4d87d4f158e
yulifu <yulifu@douyu.tv>
2018-09-25 16:40:49 +0800

    optimize log

--- log/file/Target.js
+++ log/file/Target.js
@@ -19,9 +19,10 @@ var TimeHelper = require('../../helpers/TimeHelper');
  *     'targets': {
  *         'file': {
  *             'classPath': 'y/log/file/Target',
- *             'logPath': __dirname + '/logs'
- *         },
- *         'other': {...}
+ *             'logPath': '@runtime/logs',
+ *             'logFile': 'system.log',
+ *             'maxFileSize': 10240
+ *         }
  *     },
  *     'flushInterval': 10
  * }
@@ -35,60 +36,49 @@ class Target extends ITarget {
     constructor(config) {
         super();
 
-        this.config = config;
+        /**
+         * @property {String} absolute path of log file. default at runtime directory of the application
+         */
+        this.logPath = '@runtime/logs';
 
         /**
-         * @property {String} fileExtension 文件扩展名
+         * @property {String} log file name
          */
-        this.fileExtension = undefined === config.fileExtension
-            ? '.log'
-            : config.fileExtension;
+        this.logFile = 'system.log';
 
         /**
-         * @property {String} logPath 日志路径
+         * @property {Number} maxFileSize maximum log file size in KB
          */
-        this.logPath = undefined === config.logPath
-            ? Y.getPathAlias('@runtime/logs')
-            : config.logPath;
-    }
+        this.maxFileSize = 10240;
 
-    /**
-     * 获取日志文件
-     */
-    generateFile() {
-        if(undefined !== this.config.logFile) {
-            return this.logPath + '/' + this.config.logFile;
+        // init
+        this.logPath = undefined !== config.logPath
+            ? config.logPath
+            : Y.getPathAlias(this.logPath);
+
+        if(undefined !== config.logFile) {
+            this.logFile = config.logFile;
         }
 
-        var date = new Date();
-
-        return this.logPath
-            + '/'
-            + date.getFullYear()
-            + '-'
-            + (date.getMonth() + 1)
-            + '-'
-            + date.getDate()
-            + this.fileExtension;
+        if(undefined !== config.maxFileSize) {
+            this.maxFileSize = config.maxFileSize;
+        }
     }
 
     /**
      * @inheritdoc
      */
     flush(messages) {
-        var msg = this.formatMessage(messages);
-        var file = this.generateFile();
-
         // 检查目录
         fs.access(this.logPath, fs.constants.R_OK | fs.constants.W_OK, (err) => {
             if(null === err) {
-                fs.appendFile(file, msg, Y.app.encoding, (err) => {});
+                this.writeLog(messages);
 
                 return;
             }
 
             FileHelper.createDirectory(this.logPath, 0o777, (err) => {
-                fs.appendFile(file, msg, Y.app.encoding, (err) => {});
+                this.writeLog(messages);
             });
         });
     }
@@ -100,14 +90,47 @@ class Target extends ITarget {
         var msg = '';
         for(let i=0,len=messages.length; i<len; i++) {
             msg += TimeHelper.format('y-m-d h:i:s', messages[i][2])
-                + ' -- '
+                + ' [ '
                 + Logger.getLevelName(messages[i][1])
-                + ' -- '
+                + ' ] '
                 + messages[i][0]
                 + '\n';
         }
 
         return msg;
+    }
+
+    /**
+     * 写日志
+     */
+    writeLog(messages) {
+        var msg = this.formatMessage(messages);
+        var file = this.logPath + '/' + this.logFile;
+
+        // check file exists
+        fs.access(file, fs.constants.F_OK, (err) => {
+            // file not exists
+            if(null !== err) {
+                fs.writeFile(file, msg, (err) => {});
+
+                return;
+            }
+
+            // check file size
+            fs.stat(file, (err, stats) => {
+                if(stats.size > this.maxFileSize * 1024) {
+                    let newFile = file + TimeHelper.format('ymdhis');
+
+                    fs.rename(file, newFile, (err) => {
+                        fs.writeFile(file, msg, (err) => {});
+                    });
+
+                    return;
+                }
+
+                fs.appendFile(file, msg, (err) => {});
+            });
+        });
     }
 
 }
